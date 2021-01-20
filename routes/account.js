7629e36fd804fd68e61135d6a4f6de8a269117bc
Sgray <phongnguyen6120@gmail.com>
2021-01-20 13:13:57 +0700

    render hbs

--- routes/account.js
+++ routes/account.js
@@ -1,3 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const accountController = require("../controllers/userAccountController");
+
+
+router.get("/", accountController.get);
+module.exports = router;
