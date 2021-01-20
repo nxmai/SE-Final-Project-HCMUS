02dd4858f28c2564c1626f39a6320f45ab181ffb
Sgray <phongnguyen6120@gmail.com>
2021-01-20 13:32:37 +0700

    update function for account controller

--- controllers/api/checkExistEmailController.js
+++ controllers/api/checkExistEmailController.js
@@ -3,6 +3,8 @@ const acccountServices = require("../../services/accountServices");
 
 exports.checkExistEmail  = async (req,res,next) => 
 {
-    var result = 0;
+    console.log(req.query.email);
+    let result = await acccountServices.checkExistsEmail(req.query.email);
+    console.log("check email result: " + result);
     res.status(202).send(result);
 }
