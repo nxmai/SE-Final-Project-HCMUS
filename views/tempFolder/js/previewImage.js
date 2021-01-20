4e0800e79708f4ce6c2a336734a3293ce2e7aa90
Dương Bội Long <49916875+duongboilong@users.noreply.github.com>
2021-01-20 13:43:37 +0700

    commit view in tempfolder

--- views/tempFolder/js/previewImage.js
+++
@@ -1,8 +0,0 @@
-const previewImage = function (event) {
-    let reader = new FileReader();
-    reader.onload = function () {
-      let output = document.getElementById("previewImg");
-      output.src = reader.result;
-    };
-    reader.readAsDataURL(event.target.files[0]);
-  };
