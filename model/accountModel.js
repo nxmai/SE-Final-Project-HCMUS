792ee1bf47167101f3f8e402357680cf3e378bd2
Sgray <phongnguyen6120@gmail.com>
2021-01-20 13:14:51 +0700

    thêm thành viên trong model

--- model/accountModel.js
+++ model/accountModel.js
@@ -37,3 +37,28 @@ exports.listAll = async () => {
   const users = await adminAccountCollection.find({}).toArray();
   return users;
 };
+
+// try add new user
+exports.addNewUser = async function (newUsername, plainNewPassword, newEmail) {
+  const adminAccountCollection = await db().collection("adminAccount");
+  const userpasswordCollecton = await db().collection("User-hashPassword");
+  const newInserted = await adminAccountCollection.insertOne({
+    name: newUsername,
+    age: 10,
+    email: newEmail,
+    role:"admin",
+    avatar_image: "https://res.cloudinary.com/hoavo1620/image/upload/v1610693807/q7lb7izyxsopfztremhg.jpg",
+    isVerified: false,
+    isLocked:false,
+    super_admin:false,
+  });
+  console.log("New inserted user object");
+
+  await bcrypt.hash(plainNewPassword, 3, (err, hashResult) => {
+    if (err) {
+      console.log(`Hash error: ${err}}`);
+    }
+    userpasswordCollecton.insertOne({ _id: newInserted.insertedId, password: hashResult });
+  });
+  return newInserted.insertedId;
+};
