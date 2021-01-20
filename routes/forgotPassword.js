3a8e165c82715e884c741cb24599ea153bb2c6b0
Dương Bội Long <49916875+duongboilong@users.noreply.github.com>
2021-01-20 13:00:39 +0700

    view

---
+++ routes/forgotPassword.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const forgotPasswordController = require("../controllers/forgotPasswordController");
+const router = express.Router();
+
+router.get("/", forgotPasswordController.renderForgotPasswordPage);
+router.get("/:id", forgotPasswordController.renderEnterNewPasswordPage);
+router.post("/:id", forgotPasswordController.changePassword);
+
+
+module.exports = router;
